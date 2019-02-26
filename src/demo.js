49ef8f6d884773cd21aceeb094a2b2de88363cdc
Derek Miranda <derek.miranda@ff0000.com>
2019-02-25 18:16:50 -0800

    Implements TextMeasurer, adds demo

--- src/demo.js
+++ src/demo.js
@@ -1,4 +1,14 @@
-const span = document.createElement('span')
-span.innerHTML = 'ahisfdssdfd'
+import TextMeasurer from './TextMeasurer'
 
-document.body.appendChild(span)
+window.measurer = new TextMeasurer()
+document.body.appendChild(measurer.canvas)
+const center = measurer.getCenterOfText('_-----')
+console.log('center', center)
+
+const normalizedCenter = center
+const ctx = measurer.context
+ctx.beginPath()
+ctx.strokeStyle = 'pink'
+ctx.moveTo(0, normalizedCenter)
+ctx.lineTo(measurer.canvas.width, normalizedCenter)
+ctx.stroke()
